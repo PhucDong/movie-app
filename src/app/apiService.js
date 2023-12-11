f2d4fd8c469a4326f06b85c03852ff7e93360caa
Phuc <dongvuminhphuc@gmail.com>
2023-12-11 15:17:22 +0700

    Set up axios environment

---
+++ src/app/apiService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { BASE_URL } from "./config";
+
+const apiService = axios.create({
+  baseURL: BASE_URL,
+});
+
+apiService.interceptors.request.use(
+  (request) => {
+    console.log("Start request", request);
+    return request;
+  },
+  function (error) {
+    console.log("Request error", error);
+  }
+);
+
+apiService.interceptors.response.use(
+  (response) => {
+    console.log("Response", response);
+    return response;
+  },
+  function (error) {
+    console.log("Response error", error);
+  }
+);
+
+export default apiService;
