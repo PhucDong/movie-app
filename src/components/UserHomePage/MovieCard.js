a70a030f50a93d4a1e1556ebec82c3ed8d5d7402
Phuc <dongvuminhphuc@gmail.com>
2023-12-14 15:51:14 +0700

    Add bg image and responsiveness

--- src/components/UserHomePage/MovieCard.js
+++ src/components/UserHomePage/MovieCard.js
@@ -2,15 +2,30 @@ import styled from "@emotion/styled";
 import { CardActionArea, CardContent, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import { BG_IMAGE_URL } from "../../app/config";
 
 export default function MovieCard({ categoryItem }) {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
   return (
-    <CustomStyledMovieCardActionArea component={Link} bgImage={categoryItem.pathImage}>
+    <CustomStyledMovieCardActionArea
+      component={Link}
+      bgImage={categoryItem.backdrop_path}
+      randomColor={randomColor}
+    >
+      {categoryItem.hasOwnProperty("original_title") ? (
+        <img
+          src={`${BG_IMAGE_URL}${categoryItem.backdrop_path}`}
+          alt={categoryItem.original_title}
+        />
+      ) : (
+        ""
+      )}
       <CardContent>
         <Typography>
-          {categoryItem.hasOwnProperty("movieName")
-            ? categoryItem.movieName
-            : categoryItem.genreName}
+          {categoryItem.hasOwnProperty("original_title")
+            ? categoryItem.original_title
+            : categoryItem.name}
         </Typography>
       </CardContent>
     </CustomStyledMovieCardActionArea>
@@ -18,25 +33,58 @@ export default function MovieCard({ categoryItem }) {
 }
 
 const CustomStyledMovieCardActionArea = styled(CardActionArea, {
-  shouldForwardProp: (prop) => prop !== "bgImage",
-})(({ bgImage, theme }) => ({
+  shouldForwardProp: (prop) => prop !== "bgImage" && prop !== "randomColor",
+})(({ bgImage, theme, randomColor }) => ({
   display: "inline-block",
   height: "114px",
-  backgroundImage: bgImage ? `url(${bgImage})` : "",
-  filter: "brightness(88%)",
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  "& .MuiCardContent-root": {
+  width: "200px",
+  backgroundColor: !bgImage ? `#${randomColor}` : "",
+  "& img": {
     height: "100%",
+    width: "100%",
+  },
+  "& .MuiCardContent-root": {
     display: "flex",
     justifyContent: "center",
-    alignItems: "flex-end",
-    padding: "10px",
+    alignItems: bgImage ? "flex-end" : "center",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    background:
+      "linear-gradient(rgba(42,159,255,.2) 0%, rgba(33,33,32,1) 95%, rgba(33,33,32,1) 95%)",
+    padding: "8px",
+
+    "& .MuiTypography-root": {
+      color: theme.palette.secondary.main,
+      textTransform: "capitalize",
+      fontWeight: 600,
+      fontSize: "14px",
+      lineHeight: 1.2,
+    },
   },
-  "& .MuiTypography-root": {
-    color: theme.palette.secondary.main,
-    textTransform: "capitalize",
-    fontWeight: 600,
+  [theme.breakpoints.up("sm")]: {
+    width: "220px",
+    height: "130px",
+    "& .MuiCardContent-root": {
+      "& .MuiTypography-root": {
+        fontSize: "16px",
+      },
+    },
+  },
+  [theme.breakpoints.up("md")]: {
+    width: "240px",
+    height: "150px",
+  },
+  [theme.breakpoints.up("lg")]: {
+    width: "267px",
+    height: "160px",
+    "& .MuiCardContent-root": {
+      padding: "8px 16px",
+      "& .MuiTypography-root": {
+        fontSize: "18px",
+      },
+    },
   },
 }));
