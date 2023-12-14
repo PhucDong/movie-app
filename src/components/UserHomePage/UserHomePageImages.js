de531f6a5cd2a2bc03913201471112b7955816df
Phuc <dongvuminhphuc@gmail.com>
2023-12-14 15:44:49 +0700

    Remove fake data of User Home Page

--- src/components/UserHomePage/UserHomePageImages.js
+++
@@ -1,9 +0,0 @@
-import heroSectionBgImage from "../../assets/images/UserHomePage/hero-section_bgImage.jpg";
-
-const UserHomePageImages = {
-  heroSection: {
-    pathImage: heroSectionBgImage
-  }
-}
-
-export default UserHomePageImages;
