5a086cc31abee198d45c83414f818869f14d8864
Phuc <dongvuminhphuc@gmail.com>
2024-01-05 01:30:27 +0700

    Passed data props to childre

--- src/pages/DetailedTVShowInfoPage.js
+++ src/pages/DetailedTVShowInfoPage.js
@@ -4,21 +4,21 @@ import styled from "@emotion/styled";
 import TVShowSeasonSection from "../components/DetailedTVShowInfoPage/TVShowSeasonSection";
 import CastAndCrewSection from "../components/DetailedTVShowInfoPage/CastAndCrewSection";
 import SimilarTVShowsSection from "../components/DetailedTVShowInfoPage/SimilarTVShowsSection";
-import { useEffect, useState } from "react";
 
 export default function DetailedInfoPage() {
   let tVShowDetailsLocalData = JSON.parse(
-    localStorage.getItem("tvShowDetails")
+    localStorage.getItem("tVShowDetails")
   );
-  console.log(13, tVShowDetailsLocalData);
 
   return (
     <>
       <UserPageHeroSection heroSectionData={tVShowDetailsLocalData} />
       <CustomStyledDetailedTVShowOtherInfo>
-        <TVShowSeasonSection />
-        <CastAndCrewSection />
-        <SimilarTVShowsSection />
+        <TVShowSeasonSection tVShowSeasonsData={tVShowDetailsLocalData} />
+        <CastAndCrewSection
+          castAndCrewData={tVShowDetailsLocalData.credits.cast}
+        />
+        <SimilarTVShowsSection similarTVShowsData={tVShowDetailsLocalData} />
       </CustomStyledDetailedTVShowOtherInfo>
     </>
   );
