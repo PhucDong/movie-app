fa98d3f2207b6082e377d3c9847a601497d31f42
Phuc <dongvuminhphuc@gmail.com>
2024-01-07 23:21:43 +0700

    Use useParams to fect & render data accordingly

--- src/pages/DetailedTVShowInfoPage.js
+++ src/pages/DetailedTVShowInfoPage.js
@@ -4,12 +4,41 @@ import styled from "@emotion/styled";
 import TVShowSeasonSection from "../components/DetailedTVShowInfoPage/TVShowSeasonSection";
 import CastAndCrewSection from "../components/DetailedTVShowInfoPage/CastAndCrewSection";
 import SimilarTVShowsSection from "../components/DetailedTVShowInfoPage/SimilarTVShowsSection";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import apiService from "../app/apiService";
+import { API_KEY } from "../app/config";
 
 export default function DetailedInfoPage() {
   const [tVShowDetailsLocalData, setTVShowDetailsLocalData] = useState(
     JSON.parse(localStorage.getItem("tVShowDetails"))
   );
+
+  const { tVShowId } = useParams();
+
+  useEffect(() => {
+    const fetchedTVShowDetailsWithParameter = async () => {
+      try {
+        await apiService
+          .get(
+            `/3/tv/${tVShowId}?api_key=${API_KEY}&append_to_response=credits`
+          )
+          .then((response) => {
+            localStorage.setItem(
+              "tVShowDetails",
+              JSON.stringify(response.data)
+            );
+            setTVShowDetailsLocalData(
+              JSON.parse(localStorage.getItem("tVShowDetails"))
+            );
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchedTVShowDetailsWithParameter();
+  }, [tVShowId]);
 
   return (
     <>
