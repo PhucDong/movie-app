dc6a019bcd94b178ac5ab05a136ccb24e01c8e8d
Phuc <dongvuminhphuc@gmail.com>
2024-01-07 16:34:06 +0700

    Remove some states and add extre show categories

--- src/pages/UserHomePage.js
+++ src/pages/UserHomePage.js
@@ -15,13 +15,6 @@ export default function UserHomePage() {
     genres: [],
     overview: "",
   });
-  const [topTVShowsData, setTopTVShowsData] = useState([]);
-  const [tVShowGenresData, setTVShowGenresData] = useState([]);
-  const [actionAdventureShowsData, setActionAdventureShowsData] = useState([]);
-  const [crimeShowsData, setCrimeShowsData] = useState([]);
-  const [animationShowsData, setAnimationShowsData] = useState([]);
-  const [comedyShowsData, setComedyShowsData] = useState([]);
-
   const navigate = useNavigate();
 
   const handleKeyDown = (e) => {
@@ -39,37 +32,53 @@ export default function UserHomePage() {
       heading: "Top TV Shows",
       headingTextColor: "secondary",
       bgColor: "primary",
-      categoryItemsList: topTVShowsData,
     },
     {
-      heading: "Browse by Genre",
+      heading: "Browse by Genres",
       headingTextColor: "info",
       bgColor: "secondary",
-      categoryItemsList: tVShowGenresData,
     },
     {
+      categoryId: 10759,
       heading: "Action & Adventure",
       headingTextColor: "secondary",
       bgColor: "primary",
-      categoryItemsList: actionAdventureShowsData,
     },
     {
+      categoryId: 80,
       heading: "Crime",
       headingTextColor: "info",
       bgColor: "secondary",
-      categoryItemsList: crimeShowsData,
     },
     {
+      categoryId: 16,
       heading: "Animation",
       headingTextColor: "secondary",
       bgColor: "primary",
-      categoryItemsList: animationShowsData,
     },
     {
+      categoryId: 35,
       heading: "Comedy",
       headingTextColor: "info",
       bgColor: "secondary",
-      categoryItemsList: comedyShowsData,
+    },
+    {
+      categoryId: 99,
+      heading: "Documentary",
+      headingTextColor: "secondary",
+      bgColor: "primary",
+    },
+    {
+      categoryId: 10764,
+      heading: "Reality",
+      headingTextColor: "info",
+      bgColor: "secondary",
+    },
+    {
+      categoryId: 10765,
+      heading: "Sci-Fi & Fantasy",
+      headingTextColor: "secondary",
+      bgColor: "primary",
     },
   ];
 
@@ -78,7 +87,7 @@ export default function UserHomePage() {
     const fetchedHeroSectionData = async () => {
       try {
         await apiService
-          .get(`/3/tv/40?api_key=${API_KEY}`)
+          .get(`/3/tv/400?api_key=${API_KEY}`)
           .then((response) => setHeroSectionData({ ...response.data }));
       } catch (error) {
         console.log(error);
@@ -88,106 +97,16 @@ export default function UserHomePage() {
     fetchedHeroSectionData();
   }, []);
 
-  // Get top TV Shows data
-  useEffect(() => {
-    const fetchedTopTVShowsData = async () => {
-      try {
-        await apiService
-          .get(`/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
-          .then((response) => setTopTVShowsData([...response.data.results]));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedTopTVShowsData();
-  }, []);
-
-  // Get TV Show genres data
-  useEffect(() => {
-    const fetchedTVShowGenresData = async () => {
-      try {
-        await apiService
-          .get(`/3/genre/tv/list?api_key=${API_KEY}`)
-          .then((response) => setTVShowGenresData([...response.data.genres]));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedTVShowGenresData();
-  }, []);
-
-  // Get action & adventure TV Shows data
-  useEffect(() => {
-    const fetchedActionAdventureShowsData = async () => {
-      try {
-        await apiService
-          .get(`/3/discover/tv?api_key=${API_KEY}&with_genres=10759&page=1`)
-          .then((response) =>
-            setActionAdventureShowsData([...response.data.results])
-          );
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedActionAdventureShowsData();
-  }, []);
-
-  // Get crime TV Shows data
-  useEffect(() => {
-    const fetchedCrimeShowsData = async () => {
-      try {
-        await apiService
-          .get(`/3/discover/tv?api_key=${API_KEY}&with_genres=80&page=1`)
-          .then((response) => setCrimeShowsData([...response.data.results]));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedCrimeShowsData();
-  }, []);
-
-  // Get animation TV Shows data
-  useEffect(() => {
-    const fetchedAnimationShowsData = async () => {
-      try {
-        await apiService
-          .get(`/3/discover/tv?api_key=${API_KEY}&with_genres=16&page=1`)
-          .then((response) =>
-            setAnimationShowsData([...response.data.results])
-          );
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedAnimationShowsData();
-  }, []);
-
-  // Get comedy TV Shows data
-  useEffect(() => {
-    const fetchedComedyShowsData = async () => {
-      try {
-        await apiService
-          .get(`/3/discover/tv?api_key=${API_KEY}&with_genres=35&page=1`)
-          .then((response) => setComedyShowsData([...response.data.results]));
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchedComedyShowsData();
-  }, []);
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <UserPageHeroSection heroSectionData={heroSectionData} />
       <UserPageSearchBar handleKeyDown={handleKeyDown} />
       {movieCategoryData.map((tVShowCategory, index) => (
-        <UserPageMovieCategory key={index} tVShowCategory={tVShowCategory} />
+        <UserPageMovieCategory
+          key={index}
+          tVShowCategory={tVShowCategory}
+          tVShowCategoryId={tVShowCategory.categoryId}
+        />
       ))}
     </Box>
   );
